4612b23d32ae1cced087ae8e251ae3f49abda980
julianfssen <julianfssen@gmail.com>
2024-07-26 00:51:30 +0800

    Add airdrop logic and test

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,39 @@
 use {
-    solana_sdk::{signer::keypair::Keypair, signer::Signer},
+    solana_client::rpc_client::RpcClient,
+    solana_sdk::{
+        native_token::LAMPORTS_PER_SOL,
+        signer::keypair::{Keypair, read_keypair_file},
+        signer::Signer
+    },
     crate::cli::{bs58_to_wallet::bs58_to_wallet, wallet_to_bs58::wallet_to_bs58}
 };
 
 pub mod cli;
 
+const RPC_URL: &str = "https://api.devnet.solana.com";
+
 pub fn keygen() {
-    let kp = Keypair::new();
-    println!("You've generated a new Solana wallet: {}", kp.pubkey().to_string());
+    let keypair = Keypair::new();
+    println!("You've generated a new Solana wallet: {}", keypair.pubkey().to_string());
     println!("");
     println!("To save your wallet, copy and paste the following into a JSON file:");
-    println!("{:?}", kp.to_bytes());
+    println!("{:?}", keypair.to_bytes());
+}
+
+pub fn airdrop() {
+    let keypair = read_keypair_file("dev-wallet.json").expect("Couldn't find wallet file");
+
+    println!("Airdropping 2 SOL to {:?}", keypair.pubkey().to_string());
+    let client = RpcClient::new(RPC_URL);
+
+    let sol_to_claim = 2;
+    match client.request_airdrop(&keypair.pubkey(), sol_to_claim * LAMPORTS_PER_SOL) {
+        Ok(s) => {
+            println!("Success! Check out your TX here:");
+            println!("https://explorer.solana.com/tx/{}?cluster=devnet", s.to_string());
+        },
+        Err(e) => println!("Oops, something went wrong: {}", e.to_string())
+    };
 }
 
 #[cfg(test)]
@@ -23,7 +46,10 @@ mod tests {
     }
 
     #[test]
-    fn airdrop() {}
+    fn test_airdrop() {
+        airdrop();
+    }
+
     #[test]
     fn transfer_sol() {}
 

2c36b8d9d4fd6db0a89d5354b2dd338706a09a17
julianfssen <julianfssen@gmail.com>
2024-07-25 22:48:19 +0800

    Add keygen() function and test

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,22 @@
+use solana_sdk::{signer::keypair::Keypair, signer::Signer};
+
+pub fn keygen() {
+    let kp = Keypair::new();
+    println!("You've generated a new Solana wallet: {}", kp.pubkey().to_string());
+    println!("");
+    println!("To save your wallet, copy and paste the following into a JSON file:");
+    println!("{:?}", kp.to_bytes());
+}
+
 #[cfg(test)]
 mod tests {
-    use solana_sdk;
+    use super::*;
 
     #[test]
-    fn keygen() {}
+    fn test_keygen() {
+        keygen();
+    }
+
     #[test]
     fn airdrop() {}
     #[test]
